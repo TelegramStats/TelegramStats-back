f25fdaf914a74efff4932d9447211949c7aced37
fxhxyz4 <fxhxyz@proton.me>
2023-02-26 16:02:25 +0200

    -

---
+++ server/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const colors = require('colors');
+const app = express();
+
+const PORT = process.env.PORT || 4832;
+
+app.listen(PORT, () => {
+	console.debug(`Server running on PORT: ${PORT}`.rainbow);
+});
+
+app.get('/', (req, res) => {
+	res.send('root');
+});
+
+app.get('*', (req, res) => {
+	res.send('../404.html');
+});
